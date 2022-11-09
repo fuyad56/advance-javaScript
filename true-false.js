365cc1708890fefce39b6a1ce9fa275b9aeed093
Fuyad <fuyad56@gmail.com>
2022-11-09 17:30:10 +0600

    Second

---
+++ true-false.js
@@ -0,0 +1,24 @@
+// falsely values:
+
+// o
+// ""
+// undefined
+// null
+// Nan
+// false
+
+// Truthy values:
+// "0", " ", [], {}
+
+const name = {};
+
+if (name) {
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is flase");
+}
+
+
+let name2;
+console.log(name2);
